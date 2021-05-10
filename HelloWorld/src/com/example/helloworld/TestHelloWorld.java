2a79e2e8309ce894e65915eaf9a07da059100cf0
Bilal Ahmad <57675296+bilal-github@users.noreply.github.com>
2021-05-09 19:48:54 -0600

    test Class added

---
+++ HelloWorld/src/com/example/helloworld/TestHelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestHelloWorld {
+
+    @Test
+    void shouldShowSimpleAssertion(){
+        Assertions.assertEquals(1,2);
+
+    }
+}
